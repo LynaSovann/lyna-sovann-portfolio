b2d5d86f0182e5a8f0a06369a2244870f00c9b2b
LynaSovann <sovannlyna2004@gmail.com>
2025-07-04 13:47:58 +0700

    test

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lynasovann.site', // Replace with your deployed URL
+  generateRobotsTxt: true, // Generates robots.txt file
+}
