b1cd07429d9c41d514236ec7d930cfb88a10d575
LynaSovann <sovannlyna2004@gmail.com>
2025-09-19 09:26:41 +0700

    update

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -2,10 +2,11 @@ import { Hero } from "@/components/pages/home/hero";
 import { Skills } from "@/components/pages/home/skills";
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Skills />
-    </main>
-  );
+  // return (
+  //   <main>
+  //     <Hero />
+  //     <Skills />
+  //   </main>
+  // );
+  return <Hero/>
 }
